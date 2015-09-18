c8ac0f95c554bf30b3e501391e2e6c6c69eb04cb
William Martínez <willymarpuk@gmail.com>
2015-09-18 16:22:56 -0400

    Modificaciones a las vistas de tipos de movimientos y agregue validaciones lado servidor para tipos de movimientos

--- caja_banco/app/models/tipo_de_movimiento.rb
+++ caja_banco/app/models/tipo_de_movimiento.rb
@@ -1,11 +1,21 @@
 class TipoDeMovimiento < ActiveRecord::Base
+	#has_many: movimientosdecajas
+	#has_many: movimientosdebancos
 
 
 	validates :descripcion,
 			:presence => {
-				message: 'este campo es requerido'},
+				message: 'Este campo es requerido'},
 
 			:length => { 
 				:maximum => 20,
-				:too_long => 'debe tener como maximo %{count} caracteres'}
+				:too_long => 'debe tener como maximo %{count} caracteres'},
+
+			:format => {
+				with: /\A[a-zA-Z]+\z/,
+				message: 'Solo permite letras'},
+
+			:uniqueness => {
+				message: 'Este tipo de movimiento ya existe'
+			}
 end
