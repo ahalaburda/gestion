6a1716028bee66d65776d8b9338990705c3fe98b
Adrian <adh761@gmail.com>
2015-09-22 22:45:47 -0400

    se agrego datatable y kaminari para paginacion

--- caja_banco/app/assets/javascripts/application.js
+++ caja_banco/app/assets/javascripts/application.js
@@ -12,11 +12,15 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require bootstrap-sprockets
 // require turbolinks
-//= require_tree .
 //= require rails.validations
 //= require rails.validations.simple_form
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require_tree .
 
 $(document).ready(function() {
 	$("#gbtn1 , #btn1").click(function(){
@@ -28,6 +32,17 @@ $(document).ready(function() {
 			$('#gbtn1').removeClass('glyphicon glyphicon-chevron-up');
 			$('#gbtn1').addClass('glyphicon glyphicon-plus');
 		}
+	});
+	$('#table').DataTable({
+	  // ajax: ...,
+	  // autoWidth: false,
+	  // pagingType: 'full_numbers',
+	  // processing: true,
+	  // serverSide: true,
+
+	  // Optional, if you want full pagination controls.
+	  // Check dataTables documentation to learn more about available options.
+	  // http://datatables.net/reference/option/pagingType
 	});
 });
 
