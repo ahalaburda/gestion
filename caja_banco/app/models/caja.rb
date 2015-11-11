4f53f38b6fef53ad309d13b114b4f34a139b3cd3
Adrian <adh761@gmail.com>
2015-11-11 20:44:54 -0300

    se agrego validaciones del abm de caja

--- caja_banco/app/models/caja.rb
+++ caja_banco/app/models/caja.rb
@@ -1,4 +1,20 @@
 class Caja < ActiveRecord::Base
   belongs_to :estado
   belongs_to :persona
+
+  validates :estado_id,
+		:presence => {
+			message: 'Este campo es requerido'}
+	validates :persona_id,
+		:presence => {
+			message: 'Este campo es requerido'}
+	validates :apertura,
+		:presence => {
+			message: 'Este campo es requerido'}
+	validates :saldo_inicial_efectivo,
+	 :presence => {
+	 	message: 'Este campo es requerido'}
+	validates :saldo_inicial_cheque,
+	 :presence => {
+	 	message: 'Este campo es requerido'}
 end
