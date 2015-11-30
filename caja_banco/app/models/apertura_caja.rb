15724d11255e3822d29e1ebc06a5f32cc0cb71a1
Adrian <adh761@gmail.com>
2015-11-30 19:16:10 -0300

    se realizaron mejoras en el abm de movimientos de caja y en el sistema de apertura

--- caja_banco/app/models/apertura_caja.rb
+++ caja_banco/app/models/apertura_caja.rb
@@ -4,11 +4,20 @@ class AperturaCaja < ActiveRecord::Base
   belongs_to :caja
 
   after_create :set_estado_abierto
+  before_create :set_saldos
+  before_create :set_hora_apertura
 
   def set_estado_abierto
   	caja = Caja.find(self.caja_id)
   	caja.estado_id = 1
   	caja.save
+  end
+  def set_saldos
+		self.saldo_final_cheque = self.saldo_inicial_cheque
+		self.saldo_final_efectivo = self.saldo_inicial_efectivo
+  end
+  def set_hora_apertura
+  	self.apertura = Time.now
   end
 
 end
