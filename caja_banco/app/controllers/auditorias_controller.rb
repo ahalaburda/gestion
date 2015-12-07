fc05e3e87dae365d6c06458505310f85b3f1d40a
Adrian <adh761@gmail.com>
2015-12-07 20:56:43 -0300

    pruebas para utilizar el current user

--- caja_banco/app/controllers/auditorias_controller.rb
+++ caja_banco/app/controllers/auditorias_controller.rb
@@ -1,5 +1,5 @@
 class AuditoriasController < ApplicationController
-  before_action :set_auditoria, only: [:show, :edit, :update, :destroy]
+
 
   # GET /auditorias
   # GET /auditorias.json
@@ -9,60 +9,6 @@ class AuditoriasController < ApplicationController
       format.html # index.html.erb
       format.json { render json: @auditorias }
       format.pdf { render_auditoria_list(@auditorias) }
-    end
-  end
-
-  # GET /auditorias/1
-  # GET /auditorias/1.json
-  def show
-  end
-
-  # GET /auditorias/new
-  def new
-    @auditoria = Auditoria.new
-  end
-
-  # GET /auditorias/1/edit
-  def edit
-  end
-
-  # POST /auditorias
-  # POST /auditorias.json
-  def create
-    @auditoria = Auditoria.new(auditoria_params)
-    
-    respond_to do |format|
-      if @auditoria.save
-        format.html { redirect_to @auditoria, notice: 'Auditoria was successfully created.' }
-        format.json { render :show, status: :created, location: @auditoria }
-      else
-        format.html { render :new }
-        format.json { render json: @auditoria.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /auditorias/1
-  # PATCH/PUT /auditorias/1.json
-  def update
-    respond_to do |format|
-      if @auditoria.update(auditoria_params)
-        format.html { redirect_to @auditoria, notice: 'Auditoria was successfully updated.' }
-        format.json { render :show, status: :ok, location: @auditoria }
-      else
-        format.html { render :edit }
-        format.json { render json: @auditoria.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /auditorias/1
-  # DELETE /auditorias/1.json
-  def destroy
-    @auditoria.destroy
-    respond_to do |format|
-      format.html { redirect_to auditorias_url, notice: 'Auditoria was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- caja_banco/app/models/auditoria.rb
+++ caja_banco/app/models/auditoria.rb
@@ -1,9 +1,2 @@
 class Auditoria < ActiveRecord::Base
-	before_create :set_current_user
-	# after_update :set_current_user
-
-	def set_current_user
-		self.nombre_usuario = current_user.username
-		self.save
-	end
 end
