391984225cb07b288c341ffe60a4d6f842394af6
Adrian <adh761@gmail.com>
2015-12-17 13:01:22 -0300

    cambio de estado una vez cerrada la caja

--- caja_banco/app/models/apertura_caja.rb
+++ caja_banco/app/models/apertura_caja.rb
@@ -2,14 +2,21 @@ class AperturaCaja < ActiveRecord::Base
   belongs_to :caja
   belongs_to :persona
   
-
   after_create :set_estado_abierto
   before_create :set_saldos
-  before_create :set_hora_apertura
+  before_create :set_hora_de_apertura
+  after_update :set_estado_cerrado
+  after_update :set_hora_de_cierre
 
   def set_estado_abierto
     caja = Caja.find(self.caja_id)
     caja.estado_id = 1
+    caja.save
+  end
+
+  def set_estado_cerrado
+    caja = Caja.find(self.caja_id)
+    caja.estado_id = 2
     caja.save
   end
 
@@ -18,8 +25,11 @@ class AperturaCaja < ActiveRecord::Base
     self.saldo_final_efectivo = self.saldo_inicial_efectivo
   end
 
-  def set_hora_apertura
+  def set_hora_de_apertura
     self.apertura = Time.zone.now
+  end
+  def set_hora_de_cierre
+    self.cierre = Time.zone.now
   end
 
 end
