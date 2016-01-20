d9666ed080b44b908309e9c42297c05f8782ef80
Adrian <adh761@gmail.com>
2016-01-20 18:37:57 -0300

    mejoras de identacion y definicion de metodo de retorno de tipo string

--- caja_banco/app/models/chequera.rb
+++ caja_banco/app/models/chequera.rb
@@ -4,23 +4,26 @@ class Chequera < ActiveRecord::Base
   belongs_to :estado
   has_many :cheques_propios
 
-  	validates :cuenta_bancaria_id,
-				:presence => {
-					message: 'Debe seleccionar una cuenta bancaria'}
+	validates :cuenta_bancaria_id,
+		:presence => {
+			message: 'Debe seleccionar una cuenta bancaria'}
 
- 	validates :estado_id,
-				:presence => {
-					message: 'Debe seleccionar un estado'}
+	validates :estado_id,
+		:presence => {
+			message: 'Debe seleccionar un estado'}
 
 	validates :numero_cheque_inicial,
 		:presence => {
 			message: 'Este campo es requerido'},
 		:numericality => true,
-    :length => {maximum: 30}
+		:length => {maximum: 30}
 
 	validates :numero_cheque_final,
 		:presence => {
 			message: 'Este campo es requerido'},
 		:numericality => true,
-    :length => {maximum: 30}
+		:length => {maximum: 30}
+	def nombre
+		"#{numero_cheque} - #{monto}"
+	end
 end
