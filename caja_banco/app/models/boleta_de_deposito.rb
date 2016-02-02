0d6c43ad7c01a8fc1143e89e9d3ef6e5015a2d59
Adrian <adh761@gmail.com>
2016-02-02 19:58:24 -0300

    se agregaron nuevas relaciones y las funciones para agregar registros automaticos en movimientos de caja y banco al realizar una boleta de deposito

--- caja_banco/app/models/boleta_de_deposito.rb
+++ caja_banco/app/models/boleta_de_deposito.rb
@@ -13,4 +13,63 @@ class BoletaDeDeposito < ActiveRecord::Base
   validates :cuenta_bancaria_id, presence: true
   validates :caja_id, presence: true
   validates :fecha, presence: true
+
+  after_create :movimiento_de_banco
+  after_create :movimiento_de_banco_detalle
+  after_create :movimiento_de_caja
+  after_create :movimiento_de_caja_detalle
+
+  def movimiento_de_banco
+    @movimiento_de_banco = MovimientoDeBanco.new({
+        :tipo_de_movimiento_id => 1,
+      :banco_id => 1,
+      :descripcion => "Segun boleta de deposito nº #{self.numero}" ,
+      :fecha => Time.zone.now,
+      :total => self.total
+    });
+    @movimiento_de_banco.save();
+  end
+  def movimiento_de_banco_detalle
+    mov = MovimientoDeBanco.last;
+    @movimiento_de_banco_detalle = MovimientoDeBancoDetalle.new({
+      :movimiento_de_banco_id => mov.id,
+      :cuenta_bancaria_id => self.cuenta_bancaria_id,
+      :monto => self.total
+    });
+    @movimiento_de_banco_detalle.save();
+  end
+  def movimiento_de_caja
+
+    @apertura = AperturaCaja.all.order("id ASC");
+    @apertura.each do |a|
+      if a.caja_id == self.caja_id
+        @movimiento_de_caja = MovimientoDeCaja.new({
+          :tipo_de_movimiento_id => 2,
+          :caja_id => self.caja_id,
+          :descripcion => "Segun boleta de deposito nº #{self.numero}" ,
+          :fecha => Time.zone.now,
+          :total => self.total,
+          :monto_total_efectivo => self.monto_total_efectivo,
+          :monto_total_cheque => self.monto_total_cheque,
+          :apertura_id => a.id
+          });
+      end
+    end
+     @movimiento_de_caja.save();
+  end
+  def movimiento_de_caja_detalle
+    mov = MovimientoDeCaja.last;
+    @boletas_de_depositos_detalles = BoletaDeDepositoDetalle.all
+    @boletas_de_depositos_detalles.each do |bodd|
+      if bodd.boleta_de_deposito_id == self.id
+        @movimiento_de_caja_detalle = MovimientoDeCajaDetalle.new({
+          :movimiento_de_caja_id =>mov.id ,
+          :cheque_entrante_id => bodd.cheque_entrante_id,
+          :monto_efectivo => bodd.monto_efectivo,
+          :monto_cheque => bodd.monto_cheque
+          });
+        @movimiento_de_caja_detalle.save();
+      end
+    end
+  end
 end
