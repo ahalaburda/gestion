d111a9fd524afcbdd090283480b5a3679ff1d0c4
Adrian <adh761@gmail.com>
2016-02-02 19:56:53 -0300

    mejoras en la identacion

--- caja_banco/app/models/boleta_de_deposito_detalle.rb
+++ caja_banco/app/models/boleta_de_deposito_detalle.rb
@@ -1,5 +1,5 @@
 class BoletaDeDepositoDetalle < ActiveRecord::Base
-  audited
-  belongs_to :boleta_de_deposito
-  belongs_to :cheque_entrante
+	audited
+	belongs_to :boleta_de_deposito
+	belongs_to :cheque_entrante
 end
