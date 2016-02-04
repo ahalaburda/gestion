55096982d03e84a7cdfd10e9a35d82ccb1d35db0
William Martínez <willymarpuk@gmail.com>
2016-02-04 20:39:13 -0300

    Se realizo el calculo para aumento y disminucion del saldo, en forma automatica, de una cuenta bancaria

--- caja_banco/app/models/movimiento_de_banco_detalle.rb
+++ caja_banco/app/models/movimiento_de_banco_detalle.rb
@@ -5,4 +5,17 @@ class MovimientoDeBancoDetalle < ActiveRecord::Base
 
   validates :cuenta_bancaria_id, presence: true
   validates :monto, presence: true
+
+  after_create :actualizar_saldo_cuenta
+
+  def actualizar_saldo_cuenta
+    mov = MovimientoDeBanco.last
+    cuenta = CuentaBancaria.find(self.cuenta_bancaria_id)
+    if mov.tipo_de_movimiento.descripcion == 'Ingreso'
+      cuenta.saldo += mov.total
+    else
+      cuenta.saldo -= mov.total
+    end
+    cuenta.save
+  end
 end
